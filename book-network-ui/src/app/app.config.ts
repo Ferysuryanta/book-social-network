4a1dd1cb597d3faaa0b3b63ac1406bfeb6cce222
xx-freedom-xx <107397075+Ferysuryanta@users.noreply.github.com>
2024-07-17 18:33:08 +0700

    add component book-list and configure the authorization token

--- book-network-ui/src/app/app.config.ts
+++ book-network-ui/src/app/app.config.ts
@@ -2,17 +2,27 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import {HTTP_INTERCEPTORS, HttpClient, provideHttpClient, withInterceptorsFromDi} from '@angular/common/http';
 import { LoginComponent } from './pages/login/login.component';
 import {RegisterComponent} from "./pages/register/register.component";
 import {ActivateAccountComponent} from "./pages/activate-account/activate-account.component";
+import {httpTokenInterceptor} from "./services/interceptor/http-token.interceptor";
+import {TokenService} from "./services/token/token.service";
+import {AuthenticationService} from "./services/services/authentication.service";
 
-export const appConfig: ApplicationConfig = {
+export let appConfig: ApplicationConfig;
+appConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
     LoginComponent,
     RegisterComponent,
-    ActivateAccountComponent
+    ActivateAccountComponent,
+    HttpClient,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: httpTokenInterceptor,
+      multi: true
+    }
   ]
 };
--- book-network-ui/src/app/services/token/token.service.ts
+++ book-network-ui/src/app/services/token/token.service.ts
@@ -4,7 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class TokenService {
-
     set token(token : string) {
       localStorage.setItem('token',token);
     }
